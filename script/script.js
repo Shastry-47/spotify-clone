13f8acf86317a32e8719b8f835fa59057715e09e
Shastry-47 <155943297+Shastry-47@users.noreply.github.com>
2024-10-07 22:24:14 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -13,26 +13,16 @@ function secondsToMinutes(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`https://shastry-47.github.io/spotify-clone/${folder}`);
-    let response = await a.text();
-    console.log(response);
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    songs = [];
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`${folder}`)[1]);
-        }
-    }
+    let response = await fetch(`https://shastry-47.github.io/spotify-clone/songs.json`);
+    let data = await response.json();
+    songs = data[folder];
 
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
     songUL.innerHTML = "";
     for (const song of songs) {
         songUL.innerHTML += `<li><img class="invert" src="music.svg" alt=""> 
                                 <div class="info">
-                                    <div> ${song.replaceAll("%20", " ")}</div>
+                                    <div> ${song.title}</div>
                                 </div>    
                                 <div class="playnow">
                                     <span>Play Now</span>
@@ -41,16 +31,16 @@ async function getSongs(folder) {
                             </li>`;
     }
 
-    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach((e, index) => {
+        e.addEventListener("click", () => {
+            playMusic(songs[index].file);
         });
     });
     return songs;
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `${currFolder}` + decodeURI(track);
+    currentSong.src = `${currFolder}/${track}`;
     if (!pause) {
         currentSong.play();
         play.src = "images/pause.svg";
@@ -59,41 +49,35 @@ const playMusic = (track, pause = false) => {
     document.querySelector(".songTime").innerHTML = "00:00 / 00:00";
 }
 
+
 async function displayAlbums() {
-    let a = await fetch(`https://shastry-47.github.io/spotify-clone/songs/`);
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a");
+    let response = await fetch(`https://shastry-47.github.io/spotify-clone/songs.json`);
+    let data = await response.json();
     let cardContainer = document.querySelector(".cardContainer");
-    let array = Array.from(anchors);
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index];
-        if (e.href.includes("/songs")) {
-            let folder = e.href.split("/").slice(-2)[0];
-            let a = await fetch(`https://shastry-47.github.io/spotify-clone/songs/${folder}/info.json`);
-            let response = await a.json();
-            cardContainer.innerHTML += `<div data-folder="${folder}/" class="card">
-                <div class="play">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="37" height="49" fill="none">
-                        <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1.5" fill="green" />
-                        <path d="M9.5 11.1998V12.8002C9.5 14.3195 9.5 15.0791 9.95576 15.3862C10.4115 15.6932 11.0348 15.3535 12.2815 14.6741L13.7497 13.8738C15.2499 13.0562 16 12.6474 16 12C16 11.3526 15.2499 10.9438 13.7497 10.1262L12.2815 9.32594C11.0348 8.6465 10.4115 8.30678 9.95576 8.61382C9.5 8.92086 9.5 9.6805 9.5 11.1998Z" fill="black" />
-                    </svg>                     
-                </div>
-                <img src="/songs/${folder}/cover.jpg" alt="">
-                <h4>${response.title}</h4>
-                <p>${response.description}</p>
-            </div>`;
-        }
+
+    for (const folder in data) {
+        let album = data[folder][0];  // Assuming the first song in the folder contains album info
+        cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
+            <div class="play">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="37" height="49" fill="none">
+                    <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1.5" fill="green" />
+                    <path d="M9.5 11.1998V12.8002C9.5 14.3195 9.5 15.0791 9.95576 15.3862C10.4115 15.6932 11.0348 15.3535 12.2815 14.6741L13.7497 13.8738C15.2499 13.0562 16 12.6474 16 12C16 11.3526 15.2499 10.9438 13.7497 10.1262L12.2815 9.32594C11.0348 8.6465 10.4115 8.30678 9.95576 8.61382C9.5 8.92086 9.5 9.6805 9.5 11.1998Z" fill="black" />
+                </svg>                     
+            </div>
+            <img src="songs/${folder}/${album.cover}" alt="">
+            <h4>${album.title}</h4>
+            <p>${album.description}</p>
+        </div>`;
     }
 
     Array.from(document.getElementsByClassName("card")).forEach(e => {
         e.addEventListener("click", async item => {
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
-            playMusic(songs[0]);
+            songs = await getSongs(item.currentTarget.dataset.folder);
+            playMusic(songs[0].file);
         });
     });
 }
+
 
 async function main() {
     await getSongs("songs/ncs/");
